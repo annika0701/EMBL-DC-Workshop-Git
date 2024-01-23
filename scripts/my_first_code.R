c1377955c40fe6c8046e5a614fff0b11e663fe98
annika0701 <annika-betzler@web.de>
2024-01-23 15:01:38 +0100

    this is my first R script

---
+++ scripts/my_first_code.R
@@ -0,0 +1,25 @@
+3 + 4
+
+weight_kg <- 55
+
+mickey <- 27
+x2 <- 3
+
+weight_kg <- 34
+if
+for
+
+my_weight_kg <- 50
+
+(my_weight <- 50)
+
+my_weight
+
+2.2 * weight_kg
+
+weight_kg <- 57
+2.2 * weight_kg
+
+weight_lb <- 2.2 * weight_kg
+
+weight_kg <- 100

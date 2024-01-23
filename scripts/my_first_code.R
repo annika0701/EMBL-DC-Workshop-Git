d45754ba31cdac1284031950c1d5634f564dcd77
annika0701 <annika-betzler@web.de>
2024-01-23 15:52:43 +0100

    I added some code lines

--- scripts/my_first_code.R
+++ scripts/my_first_code.R
@@ -21,5 +21,26 @@ weight_kg <- 57
 2.2 * weight_kg
 
 weight_lb <- 2.2 * weight_kg
+# I have to update the value of the weight_kg object
 
 weight_kg <- 100
+
+
+weight_lb
+mass <- 47.5
+age <- 122
+
+
+mass <- mass * 2.0
+age <- age - 20
+mass_index <- mass/age
+weight_kg <- sqrt(9)
+sqrt(9)
+
+round(3.14159)
+round(3.15159, digits = 2)
+round (3.14159, 2)
+round (digits =2, x=3.14159)
+round (2, 3.14159)
+round(4.1458694, 4)
+mean(1,2,3)

410ddd467bd332d9ab8e4bfd7afc83618d1b5bad
annika0701 <annika-betzler@web.de>
2024-01-24 10:06:58 +0100

    factors

--- scripts/my_second_code.R
+++ scripts/my_second_code.R
@@ -57,3 +57,47 @@ surveys[nrow(surveys), ]
 nrow(surveys)/2
 
 surveys [ nrow(surveys)/2, ]
+
+# Factors
+str(surveys)
+surveys$sex <- factor(surveys$sex)
+
+levels(surveys$sex)
+nlevels(surveys$sex)
+
+sex <- factor(c("male", "female", "female", "male"))
+sex <- factor(sex, levels = c("male", "female"))
+sex
+
+surveys$taxa <- factor(surveys$taxa)
+surveys$genus <- factor(surveys$genus)
+levels(surveys$genus)
+nlevels(surveys$genus)
+levels(surveys$taxa)
+sum(surveys$taxa == "Rabbit")
+summary(surveys)
+
+# convert factors
+as.character(sex)
+
+year_fct <- factor(c(1990, 1983, 1977, 1997))
+as.numeric(year_fct)
+as.numeric(levels(year_fct))[year_fct]
+
+# Renaming factors
+plot(surveys$sex)
+summary(surveys$sex)
+sex <- surveys$sex
+levels(sex)
+sex <- addNA(sex)
+levels(sex)
+
+levels(sex) [3] <- "undetermined"
+levels(sex)
+sex
+plot(sex)
+
+levels(sex)[1:2] <- c("female", "male")
+plot(sex)
+sex <- factor(sex, levels = c("undetermined", "female", "male"))
+plot(sex)
